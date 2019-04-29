9dc8824d74a2176b83c71f8a0e1d305c72316da7
Dolphin Hawkins <dolphin@exitzer0.com>
2019-04-29 15:51:06 -0700

    Cleaned up build scripts

--- Linden/install_stripped.sh
+++ Linden/install_stripped.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 set -e 
-STRIPPED=$(cd $PWD/../Stripped; pwd)
+SOURCE=$(cd $PWD/../Stripped; pwd)
 DEST=$(cd $1; pwd)
 EXTERNAL=$DEST/Code/External/Mono
 
+if [[ $# -eq 0 ]] ; then
+    echo "usage:  $0 <path to sansar>"
+    exit -1
+fi
+
 
 if [ -d "$EXTERNAL" ]; then
-    rsync -am --copy-unsafe-links --delete $STRIPPED/* $EXTERNAL
+    # copy files to external
+    rsync -am --copy-unsafe-links $SOURCE/* $EXTERNAL
+    # copy mono for build support
+    rsync -am $SOURCE/x64/Release/bin/mono-2.0-sgen.dll $EXTERNAL/Linux/Release/bin
+    rsync -am $SOURCE/x64/Release/bin/mono-sgen.exe  $EXTERNAL/Linux/Release/bin
+
+    # copy runtime files
     rsync -am --copy-unsafe-links --existing $EXTERNAL/Linux/Release/lib/mono/4.5/* $DEST/Runtime/Mono/lib/mono/4.5 
     rsync -am --copy-unsafe-links --existing $EXTERNAL/Linux/Release/lib/mono/gac/* $DEST/Runtime/Mono/lib/mono/gac 
-    rsync -am $STRIPPED/x64/Release/bin/mono-2.0-sgen.dll $DEST/Runtime
-    rsync -am $STRIPPED/x64/Release/bin/mono-sgen.exe $DEST/Runtime/Mono/bin
-    rsync -am $STRIPPED/Linux/Release/bin/mono-sgen $DEST/Runtime/Mono/bin
+
+    # copy runtime binaries
+    rsync -am $SOURCE/x64/Release/bin/mono-2.0-sgen.dll $DEST/Runtime
+    rsync -am $SOURCE/x64/Release/bin/mono-sgen.exe $DEST/Runtime/Mono/bin
+    rsync -am $SOURCE/Linux/Release/bin/mono-sgen $DEST/Runtime/Mono/bin
 else
-    echo "couldn't fine $EXTERNAL below $DEST"
+    echo "couldn't find $EXTERNAL below $DEST"
 fi
 
