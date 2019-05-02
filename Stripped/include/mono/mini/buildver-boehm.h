3498ed8be488965051e0e78d9c4c7931f75a90fe
booshLL <boosh@lindenlab.com>
2019-05-02 08:30:41 +0000

    Autogenerated checkin of linux built with build id 898101

--- Stripped/include/mono/mini/buildver-boehm.h
+++ Stripped/include/mono/mini/buildver-boehm.h
@@ -1,1 +1,1 @@
-const char *build_date = "Wed May  1 21:20:03 UTC 2019";
+const char *build_date = "Thu May  2 08:16:12 UTC 2019";
--- Stripped/include/mono/mini/version.h
+++ Stripped/include/mono/mini/version.h
@@ -1,1 +1,1 @@
-#define FULL_VERSION "mono-5.18.0.245-linden/7f27fd341f6"
+#define FULL_VERSION "mono-5.18.0.245-linden/560648a91f3"
