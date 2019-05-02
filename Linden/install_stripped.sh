280b2896c96f6e7ddd3375833682daee47340444
Dolphin Hawkins <dolphin@exitzer0.com>
2019-05-02 00:26:28 -0700

    Copy windows binaries to External/Linux

--- Linden/install_stripped.sh
+++ Linden/install_stripped.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 set -e 
-STRIPPED=$(cd $PWD/../Stripped; pwd)
+SOURCE=$(cd $PWD/../Stripped; pwd)
 DEST=$(cd $1; pwd)
 EXTERNAL=$DEST/Code/External/Mono
 
 
 if [ -d "$EXTERNAL" ]; then
-    rsync -am --copy-unsafe-links --delete $STRIPPED/* $EXTERNAL
+    # copy files to external
+    rsync -am --copy-unsafe-links --delete $SOURCE/* $EXTERNAL
+    # copy mono for build support
+    rsync -am $SOURCE/x64/Release/bin/mono-2.0-sgen.dll $EXTERNAL/Linux/Release/bin
+    rsync -am $SOURCE/x64/Release/bin/mono-sgen.exe  $EXTERNAL/Linux/Release/bin
+
+    rsync -am $SOURCE/x64/Debug/bin/mono-2.0-sgen.dll $EXTERNAL/Linux/Debug/bin
+    rsync -am $SOURCE/x64/Debug/bin/mono-sgen.exe  $EXTERNAL/Linux/Debug/bin
+
+    # copy runtime files
     rsync -am --copy-unsafe-links --existing $EXTERNAL/Linux/Release/lib/mono/4.5/* $DEST/Runtime/Mono/lib/mono/4.5 
     rsync -am --copy-unsafe-links --existing $EXTERNAL/Linux/Release/lib/mono/gac/* $DEST/Runtime/Mono/lib/mono/gac 
     rsync -am $STRIPPED/x64/Release/bin/mono-2.0-sgen.dll $DEST/Runtime
